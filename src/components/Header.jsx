f1a4e08e9be0390e15b3305aa6939b1172492996
Teagan Durtschi <lakewingrasoftware@gmail.com>
2025-10-17 15:35:32 -0500

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ export default function Header({ isAnonymous }) {
     <header>
       <div className="header">
         <h1>
-          <Link to="/browse">Public Bookmarks</Link>
+          <Link to="/browse">
+            Public Bookmarks
+          </Link>
+          <img src="/bookmark.svg" alt="Bookmark Icon" style={{ height: '24px', verticalAlign: 'middle', marginRight: '8px' }} />
         </h1>
         {isAnonymous ? (
           <div style={{ display: "flex", gap: "8px" }}>
