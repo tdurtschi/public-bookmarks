f5203d0905c0319fb0fb2e7e0e8ec5549de23ad9
Teagan Durtschi <lakewingrasoftware@gmail.com>
2025-10-18 14:49:05 -0500

    remove unneeded code for localhost env

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,12 +2,11 @@ import { supabase } from './supabaseClient'
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 export default function Auth2() {
-  const isDevelopment = import.meta.env.VITE_MODE === "development";
   return (
     <div className="row flex flex-center">
       <div className="col-6 form-widget">
-        <h1 className="header">Public Bookmarks - Login{isDevelopment ? " - Local" : ""}</h1>
-        <Auth supabaseClient={supabase} providers={[]} magicLink={true} redirectTo={isDevelopment ? "localhost:5173" : null} appearance={{ theme: ThemeSupa }}  />
+        <h1 className="header">Public Bookmarks - Login</h1>
+        <Auth supabaseClient={supabase} providers={[]} magicLink={true} appearance={{ theme: ThemeSupa }}  />
       </div>
     </div>
   )
